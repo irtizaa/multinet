40ae4a67002bca54dca3abe6667e7687d30c5969
irtizaa <irtizahussain.edu098@gmail.com>
2023-01-24 13:04:33 +0500

    Updates

---
+++ precache-manifest.8cd73fe4a13740733254aa67002641f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4aeef18fc004a44ab571",
+    "url": "/multinet/static/css/main.1e669a62.chunk.css"
+  },
+  {
+    "revision": "4aeef18fc004a44ab571",
+    "url": "/multinet/static/js/main.ff3141bf.chunk.js"
+  },
+  {
+    "revision": "d0ef1e110f980e69fb47",
+    "url": "/multinet/static/js/runtime~main.490938ff.js"
+  },
+  {
+    "revision": "f0385b250d6094c68a18",
+    "url": "/multinet/static/css/2.fc9aa1fe.chunk.css"
+  },
+  {
+    "revision": "f0385b250d6094c68a18",
+    "url": "/multinet/static/js/2.8dcbf5d8.chunk.js"
+  },
+  {
+    "revision": "d597820c4cf887a3b308e9d1a6bff22e",
+    "url": "/multinet/index.html"
+  }
+];
